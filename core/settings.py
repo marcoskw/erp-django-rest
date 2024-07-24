369e9e244be16a3453debe4cfa2d17a2e64730ed
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-07-24 13:38:37 -0300

    Configuring database

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 
     # Rest Framework
     'rest_framework',
-
 ]
 
 MIDDLEWARE = [
@@ -79,8 +78,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'erp',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': 3306,
     }
 }
 
