11c2b31cbce9f1019c0529a038f07428b88bf5d7
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-07-24 13:11:19 -0300

    Configuring Rest Framework in settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Rest Framework
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
