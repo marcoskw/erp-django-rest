8fe93ad0cd60d491e3c2c2f612cb5760fd8c8ab3
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-07-26 16:13:51 -0300

    Create accounts and companies Apps

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'corsheaders',
 
     # Apps
-    'auth',
+    'companies',
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
