952f955c0703ef130b16331fe1ec06e7b4929c5b
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-07-30 17:28:28 -0300

    Create TaskStatus and Task Models

--- companies/models.py
+++ companies/models.py
@@ -8,3 +8,19 @@ class Enterprise(models.Model):
 class Employee(models.Model):
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+class TaskStatus(models.Model):
+    class Meta:
+        db_table = "companies_task_status"
+    name = models.CharField(max_length=155)
+    codename = models.CharField(max_length=100)
+
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    status = models.ForeignKey(TaskStatus, on_delete = models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
